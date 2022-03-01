b5621125ce13e4f457a4f2ed916577523b6c6590
Vidushi <meelvidushi1@gmail.com>
2022-02-28 22:24:59 -0500

    edit and delete posts

--- meteordraft2/social/views.py
+++ meteordraft2/social/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
+from django.views.generic.edit import UpdateView, DeleteView
 
 
 class PostListView(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all().order_by('-created_on')
         form = PostForm()
+
 
         context = {
             'post_list': posts,
@@ -31,3 +34,30 @@ class PostListView(View):
         }
 
         return render(request, 'social/post_list.html', context)
+
+class PostDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk = pk)
+        form = CommentForm()
+
+        context = {
+            'post': post,
+            'form': form,
+        }
+
+        return render(request, 'social/post_detail.html', context)
+
+class PostEditView(UpdateView):
+    model = Post
+    fields = ['body']
+    template_name = 'social/post_edit.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('post-detail', kwargs = {'pk' : pk})
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'social/post_delete.html'
+    success_url = reverse_lazy('post-list')
+
